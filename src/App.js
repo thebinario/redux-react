018dc49127d1bd410596b30f3f9ae53abefe2d23
Abimael Ferreira <abimaelfrrr@gmail.com>
2021-02-28 20:36:34 -0300

    passando props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import React, {Component} from 'react';
+
 import Sidebar from './components/Sidebar';
 import Video from './components/Video';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Video></Video>
-      <Sidebar></Sidebar>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    activeLesson: {},
+    modules: [
+        {id:1,
+            title: 'Iniciando projeto',lessons: [
+                {id:1, title: 'inicindo aula'},
+                {id:2, title: 'instalando dependencias'},
+            ],
+        },
+        {id:2,
+            title: 'continuando projeto',lessons: [
+                {id:1, title: 'programcao'},
+                {id:2, title: 'refatorando'},
+            ],
+        },
+    ],
+}
+
+  render () {
+    return (
+      <div className="App">
+        <Video activeLesson={this.state.activeLesson}></Video>
+        <Sidebar modules={this.state.modules}></Sidebar>
+      </div>
+    );
+  }
 }
 
 export default App;
